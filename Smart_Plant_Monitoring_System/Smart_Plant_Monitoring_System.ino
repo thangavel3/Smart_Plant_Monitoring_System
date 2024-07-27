0f335884135d7e0086148459940c761a9a501588
thangavel3 <166533406+thangavel3@users.noreply.github.com>
2024-07-27 21:46:36 +0530

    Add files via upload

---
+++ Smart_Plant_Monitoring_System/Smart_Plant_Monitoring_System.ino
@@ -0,0 +1,56 @@
+
+#define BLYNK_TEMPLATE_ID "TMPL33qWvtfWV"
+#define BLYNK_TEMPLATE_NAME "Smart Plant"
+#define BLYNK_AUTH_TOKEN "gIwz0dpjXxKEhAxDz3KPho3P4mFDKWko"
+
+#define BLYNK_PRINT Serial
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+ 
+
+#include <DHT.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+char ssid[] = "Galaxy";  // type your wifi name
+char pass[] = "123456789";  // type your wifi password
+
+#define DHTPIN 2          // Mention the digital pin where you connected 
+#define DHTTYPE DHT11     // DHT 11  
+DHT dht(DHTPIN, DHTTYPE);
+BlynkTimer timer;
+
+void sendSensor(){
+  float h = dht.readHumidity();
+  float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.println(t);
+  Blynk.virtualWrite(V6, h);
+  Blynk.virtualWrite(V5, t);
+    Serial.print("Temperature : ");
+    Serial.print(t);
+    Serial.print("    Humidity : ");
+    Serial.println(h);
+
+
+  if(t >30){
+   
+    Blynk.logEvent("temp_alert","Temp above 30 degree");
+  }
+}
+
+void setup(){
+   Serial.begin(115200);
+  Blynk.begin(auth, ssid, pass);
+  dht.begin();
+  timer.setInterval(2500L, sendSensor);
+}
+
+void loop(){
+  Blynk.run();
+  timer.run();
+}
